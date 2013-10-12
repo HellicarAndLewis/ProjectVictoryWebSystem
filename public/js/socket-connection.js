ddbd4b7099ccb27b97a90f8e2dc10026c0901494
Ross Cairns <ross@electricglen.com>
2013-10-12 13:14:38 +0100

    Fix for disconnection

--- public/js/socket-connection.js
+++ public/js/socket-connection.js
@@ -5,7 +5,7 @@ function createJSONSocket() {
 
     var socketIsLive = false;
     jsonSocket.on('open', function () { socketIsLive = true; $('.connectionStatus .connected').removeClass('hidden'); $('.connectionStatus .disconnected').addClass('hidden'); } );
-    jsonSocket.on('close', function () { socketIsLive = false; $('.connectionStatus .connected').addClass('hidden'); $('.connectionStatus .disconnected').removeClass('hidden'); } );
+    jsonSocket.on('closed', function () { socketIsLive = false; $('.connectionStatus .connected').addClass('hidden'); $('.connectionStatus .disconnected').removeClass('hidden'); } );
     jsonSocket.openSocketConnection();
     return jsonSocket;
 }
